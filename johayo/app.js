b235bcc12f2ae702d504cb8db28c22c08cb97239
mayajuni@naver.com <mayajuni@naver.com>
2013-04-30 06:53:01 +0900

    수정

--- johayo/app.js
+++ johayo/app.js
@@ -7,19 +7,29 @@ var express = require('express')
 var app = express();
  
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+app.use(express.logger());
+app.use(express.cookieParser());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('ejs', engine);
  
  
-app.get('/', function(req , res, next){
+app.all('/', function(req , res, next){
+	controller.findController(req, res, next);
+});
+
+app.all('/', function(req , res, next){
+	controller.findController(req, res, next);
+});
+
+app.all('/', function(req , res, next){
 	controller.findController(req, res, next);
 });
  
--- johayo/controller/mainController.js
+++ johayo/controller/mainController.js
@@ -7,5 +7,4 @@ var mainRoute = require('../routes/mainRoute.js');
 /* main */
 exports.controller = function(req, res, next){
 	res.render('index',{title:'메인페이지'});
-	next();
 };
