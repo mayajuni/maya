a7ed931f2af703b32ea5af3e062ecc36a26c87a4
mayajuni <mayajuni@naver.com>
2013-07-23 15:22:41 +0900

    수정

--- johayo/routes/board.js
+++ johayo/routes/board.js
@@ -125,6 +125,8 @@ function boardDetail(req, res, param){
  * @returns
  */
 function boardInsert(req, res, param){
+	if(req.parma("title"))
+		parma["title"] = req.parma("title"); 
 	res.render("boardInsert", param);
 }
 
